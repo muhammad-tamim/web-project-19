be0e3c80868aa262f8eaf984ab29c64b716010a9
muhammad-tamim <contact2tamim@gmail.com>
2025-09-25 18:38:38 +0600

    feat: add right side

--- src/App.jsx
+++ src/App.jsx
@@ -14,15 +14,21 @@ const App = () => {
 
       <div className='grid grid-cols-12 mt-8 mb-52'>
         {/* left side */}
-        <div className='col-span-9'>
+        <div className='col-span-8'>
           <Suspense fallback={<span className="loading loading-spinner loading-xl text-center"></span>}>
             <DisplayData fetchData={fetchData}></DisplayData>
           </Suspense>
         </div>
 
         {/* right side */}
-        <div className='col-span-3 bg-gray-200'>
+        <div className='col-span-4'>
+          <p className='text-[#6047EC] mb-6 font-bold text-2xl border border-[#6047EC] bg-[#6047EC10] rounded-lg py-5 px-12'>Spent time on read : 177 min</p>
 
+          <div className='rounded-lg bg-[#11111105] p-7 '>
+            <h3 className='text-2xl font-bold mb-6'>Bookmarked Blogs : 8</h3>
+
+            <p className='text-lg font-semibold p-5 mb-6 rounded-lg bg-white'>Master Microsoft Power Platform and Become an In-Demand!</p>
+          </div>
         </div>
 
       </div>
