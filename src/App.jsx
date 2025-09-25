77e1011751c2525f19e1701303bb2c4d06e2ec35
muhammad-tamim <contact2tamim@gmail.com>
2025-09-25 19:05:26 +0600

    refactor: use useEffect hook intead of use hook for better controling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect, useState, } from 'react';
 import Navbar from './components/Navbar/Navbar';
 import DisplayData from './components/DisplayData/DisplayData';
 import { ErrorBoundary } from 'react-error-boundary';
 
-const fetchData = fetch('./data.json')
-  .then(res => res.json())
 
 const App = () => {
+
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    fetch('./data.json')
+      .then(res => res.json())
+      .then(data => setData(data))
+      .catch(err => setError(err))
+      .finally(() => setLoading(false))
+
+  }, [])
+
+
 
   return (
     <div className='max-w-7xl mx-auto'>
@@ -15,17 +28,21 @@ const App = () => {
       <div className='grid grid-cols-12 mt-8 mb-52'>
         {/* left side */}
         <div className='col-span-8'>
-          <Suspense fallback={<span className="loading loading-spinner loading-xl text-center"></span>}>
-            <DisplayData fetchData={fetchData}></DisplayData>
-          </Suspense>
+          {loading && <div className='text-center '>
+            <span className="loading size-16 loading-spinner"></span>
+          </div>}
+          {error && <p className='text-center text-3xl text-red-500'>{error.message}</p>}
+          <DisplayData data={data}></DisplayData>
         </div>
 
         {/* right side */}
         <div className='col-span-4'>
-          <p className='text-[#6047EC] mb-6 font-bold text-2xl border border-[#6047EC] bg-[#6047EC10] rounded-lg py-5 px-12'>Spent time on read : 177 min</p>
+          <p className='text-[#6047EC] mb-6 font-bold text-2xl border border-[#6047EC] bg-[#6047EC10] rounded-lg py-5 px-12'>
+            Spent time on read : 0 min
+          </p>
 
           <div className='rounded-lg bg-[#11111105] p-7 '>
-            <h3 className='text-2xl font-bold mb-6'>Bookmarked Blogs : 8</h3>
+            <h3 className='text-2xl font-bold mb-6'>Bookmarked Blogs : 0</h3>
 
             <p className='text-lg font-semibold p-5 mb-6 rounded-lg bg-white'>Master Microsoft Power Platform and Become an In-Demand!</p>
           </div>
--- src/components/DisplayData/DisplayData.jsx
+++ src/components/DisplayData/DisplayData.jsx
@@ -1,8 +1,6 @@
-import React, { use } from 'react';
 import Card from '../Card/Card';
 
-const DisplayData = ({ fetchData }) => {
-    const data = use(fetchData)
+const DisplayData = ({ data }) => {
     return (
         <div>
             {data.map((singleData => <Card key={singleData.id} singleData={singleData}></Card>))}
